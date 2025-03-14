d47b6a1b6fbfb84f452276759808a3d87ac72e17
lxh <lixihang@wudi360.com>
2025-03-14 11:09:53 +0800

    上传jitpack优化5

--- DKUilibrary/build.gradle.kts
+++ DKUilibrary/build.gradle.kts
@@ -11,7 +11,7 @@ afterEvaluate {
             // Creates a Maven publication called "release".
             create<MavenPublication>("release") {
                 // Applies the component for the release build variant.\
-                // from(components["release"])
+                 from(components["release"])
                 // You can then customize attributes of the publication as shown below.
                 groupId = "com.deak"
                 artifactId = "dkuseutils"
